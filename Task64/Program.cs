189bb03255fc78a761bc26c2381ab4a3e8a2ffe7
KisEA <maslennikvakat@mail.ru>
2022-09-22 18:53:59 +0300

    Добавлено решение 64 задачи

---
+++ Task64/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeTasks_KisEA
+{
+    class Program
+    {
+        static void Main (string[] args)        // Программа выводит все натуральные числа в промежутке от N до M
+        {
+            void showIntegers(int n, int m)
+            {
+                if(n < m)
+                    for(int i = n; i <= m; i ++) Console.Write($"{i} ");
+                else if(n > m)
+                    for(int i = m; i <= n; i++) Console.Write($"{i} ");
+                
+                Console.WriteLine();
+            }
+            
+            try
+            {
+                Console.WriteLine("Программа покажет все натуральные числа в промежутке от N до M");
+                Console.Write("N: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("M: ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                showIntegers(n,m);
+            }
+            catch
+            {
+                Console.WriteLine("Данные необходимо вводить цифрами");
+            }
+
+        }
+    }
+}
